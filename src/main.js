4c0ea1f2368d809ea74f8d3b35b468181d9a197f
Diego Fernandes <diego.schell.f@gmail.com>
2018-03-08 20:34:08 -0300

    Buscando dados da API

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
+import axios from 'axios';
+
 class App {
   constructor() {
     this.repositories = [];
 
     this.formEl = document.getElementById('repo-form');
+    this.inputEl = document.querySelector('input[name=repository]');
     this.listEl = document.getElementById('repo-list');
+
+    this.api = axios.create({
+      baseURL: 'https://api.github.com',
+    });
     
     this.registerListeners();
   }
@@ -13,15 +20,26 @@ class App {
       this.addRepository(event);
   }
 
-  addRepository(event) {
+  async addRepository(event) {
     event.preventDefault();
 
+    const repoInput = this.inputEl.value;
+
+    if (repoInput.length === 0) 
+      return;
+
+    const response = await this.api.get(`/repos/${repoInput}`);
+
+    const { name, description, html_url, owner: { avatar_url } } = response.data;
+
     this.repositories.push({
-      avatar_url: 'https://avatars0.githubusercontent.com/u/28929274',
-      name: 'rocketseat.com.br',
-      description: 'Tire sua ideia do papel e dê vida à sua startup',
-      html_url: 'https://github.com/RocketSeat/rocketseat.com.br',
+      avatar_url,
+      name,
+      description,
+      html_url,
     });
+
+    this.inputEl.value = '';
 
     this.render();
   }
